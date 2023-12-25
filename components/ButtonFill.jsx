34d6da006941b9539dde4675bf00d9d77e6a09cc
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2023-12-25 22:24:05 +0400

    fixed bugs

--- components/ButtonFill.jsx
+++ components/ButtonFill.jsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
+export default function ButtonFill({ children, scrollPos }) {
+    return <button onClick={ () => {
+        document.getElementById(scrollPos).scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+    } } className="py-[10px] rounded-[5px] md:py-[5px] md:px-[15px] md:text-[18px] px-[25px] font-[600] text-[20px] bg-[#ffa97a] text-[#293241] hover:bg-transparent hover:text-white transition-colors border-[2px] border-[#ffa97a]">{ children }</button>
+}
 
-export default ({ children, scrollPos }) => <button onClick={ () => {
-    document.getElementById(scrollPos).scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
-} } className="py-[10px] rounded-[5px] md:py-[5px] md:px-[15px] md:text-[18px] px-[25px] font-[600] text-[20px] bg-[#ffa97a] text-[#293241] hover:bg-transparent hover:text-white transition-colors border-[2px] border-[#ffa97a]">{ children }</button>
