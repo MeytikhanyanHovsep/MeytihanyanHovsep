5294fcfd74181a0757dcecbe89f71e479ab79103
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2023-12-26 09:42:00 +0400

    added symbol

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ function Home() {
         <div className="container">
             <motion.div initial={ { y: 100, opacity: 0 } } viewport={ { once: 1 } } whileInView={ { y: 0, opacity: 1 } } transition={ { easy: "linear", duration: 1, delay: 0.3 } } className="mx-auto my-[100px] sm:my-[50px] max-w-max sm:py-[10px] sm:px-[15px] sm:text-[16px] py-[30px] relative px-[20px] border-[2px] text-white text-[20px] rounded-[5px] border-[#ffa97a]">
                 <span className="absolute top-[-34px] left-[10px] text-[#ffa97a] sm:top-[-25px] sm:text-[50px] text-[70px]">&quot;</span>
-                A developer who doesn't hack an Instagram accounts
+                A developer who doesn&quot;t hack an Instagram accounts
                 <span className="absolute bottom-[-75px] right-[10px] text-[#ffa97a] sm:bottom-[-53px] sm:text-[50px] text-[70px]">&quot;</span>
             </motion.div>
         </div>
